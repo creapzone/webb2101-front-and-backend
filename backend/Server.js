a2c2da6d4192990249e96759e98b23a5208a415f
cmenta97 <77394560+cmenta97@users.noreply.github.com>
2021-09-09 13:45:28 +0200

    Lade till CORS

--- backend/Server.js
+++ backend/Server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
 
 // Här finns databas relaterat
 let currentId = 14
