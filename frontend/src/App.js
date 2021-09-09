4321438c4a2f80f2d4567f9189a676fe9baf907d
cmenta97 <77394560+cmenta97@users.noreply.github.com>
2021-09-09 14:07:19 +0200

    Gjorde alla GET anrop via axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,59 @@
 import './App.css';
 import { useState } from "react";
+import http from './utils/api/UsersApi'
 
 function App() {
     const [text, setText] = useState('paragraph')
+
+    function alive() {
+        http.get('/')
+            .then(function (response) {
+                if (response.status === 200) {
+                    console.log(response.data)
+                    setText(response.data)
+                }
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error)
+                return 'Error'
+            })
+            .then(function () {
+                // always executed
+
+            })
+    }
+
+    function getUsers() {
+        http.get('/users')
+            .then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+    function getUserById(id) {
+        http.get(`/users/${ id }`)
+            .then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
 
     return (
     <div>
         <h1>Users API with Axios</h1>
         <h3>'npm install axios'</h3>
         <p>{ text }</p>
+
         <button onClick={() => {setText('New text')}}>New text</button>
+        <button onClick={ alive }>Alive</button>
+        <button onClick={ getUsers }>Get Users</button>
+        <button onClick={ function() { getUserById(10)} }>Get user by id</button>
     </div>
   );
 }
