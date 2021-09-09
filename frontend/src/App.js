136f820b6b27317b41ee109afd80075a6a1915ed
cmenta97 <77394560+cmenta97@users.noreply.github.com>
2021-09-09 14:38:21 +0200

    La till POST request för att skapa användare

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from "react";
+import {useState} from "react";
 import http from './utils/api/UsersApi'
 
 function App() {
@@ -35,7 +35,22 @@ function App() {
     }
 
     function getUserById(id) {
-        http.get(`/users/${ id }`)
+        http.get(`/users/${id}`)
+            .then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+    function createUser(name, age, gender) {
+        const payload = {
+            "name": name,
+            "age": age,
+            "gender": gender
+        }
+        http.post('/users', payload)
             .then(function (response) {
                 console.log(response.data)
             })
@@ -45,17 +60,27 @@ function App() {
     }
 
     return (
-    <div>
-        <h1>Users API with Axios</h1>
-        <h3>'npm install axios'</h3>
-        <p>{ text }</p>
+        <div>
+            <h1>Users API with Axios</h1>
+            <h3>'npm install axios'</h3>
+            <p>{text}</p>
 
-        <button onClick={() => {setText('New text')}}>New text</button>
-        <button onClick={ alive }>Alive</button>
-        <button onClick={ getUsers }>Get Users</button>
-        <button onClick={ function() { getUserById(10)} }>Get user by id</button>
-    </div>
-  );
+            <button onClick={() => {
+                setText('New text')
+            }}>New text
+            </button>
+            <button onClick={alive}>Alive</button>
+            <button onClick={getUsers}>Get Users</button>
+            <button onClick={function () {
+                getUserById(14)
+            }}>Get user by id
+            </button>
+            <button onClick={function () {
+                createUser('Ada', 18, 'female')
+            }}>Create user
+            </button>
+        </div>
+    );
 }
 
 export default App;
